e23afdd4171082ee0d97bffbffb9dfec508dd4b1
Lisa Lee <lisaleekallman@gmail.com>
2021-07-11 12:04:03 +0200

    Updatex text content, redesigned photo gallery, added pop up modal for bandDagen psoter

--- js/main.js
+++ js/main.js
@@ -7,59 +7,73 @@
 // function newpage() {
 //     window.location = newLocation;
 // }
+document.addEventListener(
+    "click",
+    function (event) {
+        if (
+            event.target.matches(".close") ||
+            !event.target.closest(".popup-content")
+        ) {
+            closeModal()
+        }
+    },
+    false
+)
 
+function closeModal() {
+    document.querySelector(".popup").style.display = "none"
+}
 
+$(document).ready(function () {
 
-$(document).ready(function(){
-
-// /*! Fades in whole page on load */
+    // /*! Fades in whole page on load */
     $('body').css('display', 'none');
     $('body').fadeIn(1000);
 
-}); 
+});
 
 /*! Reloads on every visit in mobile safari */
 if ((/iphone|ipod|ipad.*os 5/gi).test(navigator.appVersion)) {
-    window.onpageshow = function(evt) {
+    window.onpageshow = function (evt) {
 
-    if (evt.persisted) {
-        document.body.style.display = "none";
-        location.reload();
+        if (evt.persisted) {
+            document.body.style.display = "none";
+            location.reload();
         }
     };
 }
 
 /*! Reloads page on every visit */
 function Reload() {
-try {
-var headElement = document.getElementsByTagName("head")[0];
-if (headElement && headElement.innerHTML)
-headElement.innerHTML += "<meta http-equiv=\"refresh\" content=\"1\">";
-}
-catch (e) {}
+    try {
+        var headElement = document.getElementsByTagName("head")[0];
+        if (headElement && headElement.innerHTML)
+            headElement.innerHTML += "<meta http-equiv=\"refresh\" content=\"1\">";
+    }
+    catch (e) { }
 }
 
 /*! smooth scrolling */
-    var ssSmoothScroll = function() {
-        
-        $('.smoothscroll').on('click', function (e) {
-            var target = this.hash,
-            $target    = $(target);
-            
-                e.preventDefault();
-                e.stopPropagation();
+var ssSmoothScroll = function () {
 
-            $('html, body').stop().animate({
-                'scrollTop': $target.offset().top
-            }, cfg.scrollDuration, 'swing').promise().done(function () {
+    $('.smoothscroll').on('click', function (e) {
+        var target = this.hash,
+            $target = $(target);
 
-                // check if menu is open
-                if ($('body').hasClass('menu-is-open')) {
-                    $('.header-menu-toggle').trigger('click');
-                }
+        e.preventDefault();
+        e.stopPropagation();
 
-                window.location.hash = target;
-            });
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, cfg.scrollDuration, 'swing').promise().done(function () {
+
+            // check if menu is open
+            if ($('body').hasClass('menu-is-open')) {
+                $('.header-menu-toggle').trigger('click');
+            }
+
+            window.location.hash = target;
         });
+    });
 
-    };
+};
